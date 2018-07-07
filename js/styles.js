da1f4b38fe6bff6d1a1ce8ecfeafd39ef994a957
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-07 18:11:25 +0300

    Added the footer

--- js/styles.js
+++ js/styles.js
@@ -1,7 +1,5 @@
-function openNav() {
-    document.getElementById("myNav").style.display = "block";
-}
-
-function closeNav() {
-    document.getElementById("myNav").style.display = "none";
-}
+$(document).ready(function(){
+  $("submit").insert(function(){
+    alert("Feedback")
+  })
+})
